2c821c2d9e346072ea77246a7deb0bc853bbae43
Morgan Johansson <morgan.johansson@me.com>
2024-09-21 15:34:19 +0200

    Added functionality to the Dark Mode toggle using javascript

---
+++ script.js
@@ -0,0 +1,35 @@
+const darkmodeSwitch = document.querySelector('#darkmode-switch')
+const hasDarkmode = localStorage.getItem('darkmode')
+
+if(hasDarkmode == null) {
+    if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        enableDarkMode()
+    } else {
+        diasableDarkMode()
+    }
+} else if(hasDarkmode === 'on') {
+    enableDarkMode()
+} else if(hasDarkmode === 'off') {
+    disableDarkMode()
+}
+
+
+darkmodeSwitch.addEventListener('change', () => {
+    if(darkmodeSwitch.checked) {
+        enableDarkMode()
+        localStorage.setItem('darkmode', 'on')
+    } else {
+        disableDarkMode()
+        localStorage.setItem('darkmode', 'off')
+    }
+})
+
+
+function enableDarkMode() {
+    darkmodeSwitch.checked = true
+    document.documentElement.classList.add('dark')
+}
+function disableDarkMode() {
+    darkmodeSwitch.checked = false
+    document.documentElement.classList.remove('dark')
+}
